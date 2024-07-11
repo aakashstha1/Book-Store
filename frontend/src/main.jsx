c132569edc9287ebb6508df1efa2b363318f5750
aakashstha1 <aakash.cerestha90@gmail.com>
2024-07-11 16:48:57 +0545

    Day41

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
+import "./scrollbar.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
